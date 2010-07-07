1aa04699fa41aff8e5dcc787326bdec39002952e
cjgb (Mr ) <cgb@datanalytics.com>
2010-07-07 00:08:27 +0000

    Included exception handling plus specialized functions for assignments and for running arbitrary code.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rjython@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/jython.call.R
+++ pkg/R/jython.call.R
@@ -1,18 +1,10 @@
 #########################################################
-# CGB, 20100703
+# CGB, 20100707, created
 #########################################################
 
 jython.call <- function( rJython, py.foo, ... ){
 
-    fix.factors <- function( x ){           # Temporary fix as rjson package cannot parse factors
-        if( is.factor( x ) )
-            return( as.character( x ) )
-        x
-    }
-
-    # foo.args <- list( ... )
-    foo.args <- fix.factors( list( ... ) )
-
+    foo.args <- list( ... )
 
     if( is.null( names( foo.args ) ) )
         which.dict <- rep( FALSE, length( foo.args ) )
@@ -27,22 +19,26 @@ jython.call <- function( rJython, py.foo, ... ){
 
     # Passing data to jython
 
-    rJython$exec( paste( "_r_args_dict ='", foo.args.dict, "'", sep = "" ) )
-    rJython$exec( paste( "_r_args_vect ='", foo.args.vect, "'", sep = "" ) )
-    rJython$exec( "_r_args_dict = json.loads( _r_args_dict )" )
-    rJython$exec( "_r_args_vect = json.loads( _r_args_vect )" )
+    #rJython$exec( paste( "_r_args_dict ='", foo.args.dict, "'", sep = "" ) )
+    #rJython$exec( paste( "_r_args_vect ='", foo.args.vect, "'", sep = "" ) )
+    #rJython$exec( "_r_args_dict = json.loads( _r_args_dict )" )
+    #rJython$exec( "_r_args_vect = json.loads( _r_args_vect )" )
 
     # Creating the call
 
-    jython.command <- paste( "_r_return = ", py.foo, "(",
-                              ifelse( n.args.vect == 1, "_r_args_vect[0]", "*_r_args_vect" ),
-                              ifelse( n.args.dict == 0, ")", ", **_r_args_dict)" ), 
-                              sep = "" )
+    jython.command <- c( 
+        paste( "_r_args_dict ='", foo.args.dict, "'", sep = "" ),
+        paste( "_r_args_vect ='", foo.args.vect, "'", sep = "" ),
+        "_r_args_dict = json.loads( _r_args_dict )",
+        "_r_args_vect = json.loads( _r_args_vect )",
+        jython.command <- paste( "_r_return = ", py.foo, "(",
+                                  ifelse( n.args.vect == 1, "_r_args_vect[0]", "*_r_args_vect" ),
+                                  ifelse( n.args.dict == 0, ")", ", **_r_args_dict)" ), 
+                                  sep = "" )
+    )
 
-
-    rJython$exec( jython.command )
+    jython.exec( rJython, jython.command )
     rJython$exec( "_r_return = json.dumps( [ _r_return ] )" )
-    
     ret <- fromJSON( .jstrVal( rJython$get ( "_r_return" )) )
 
     if( length( ret ) == 1 ) ret <- ret[[1]]
